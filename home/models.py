9ca2863aa32811bddfea27837b198cb8ba9a9b03
Rohit Pandey <croods660@gmail.com>
2024-04-18 13:37:46 +0530

    Day 26 completed

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Students(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+   
+
+
+class Products(models.Model):
+    pass
