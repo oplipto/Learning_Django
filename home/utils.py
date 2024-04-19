bc9003846b9b1d3c055a9e40d545119df4ece5fc
Rohit Pandey <croods660@gmail.com>
2024-04-19 18:55:52 +0530

    Day 27 of 100 Completed

---
+++ home/utils.py
@@ -0,0 +1,10 @@
+from home.models import Students
+
+import time
+
+def run_this_function():
+    print('Function Started')
+    print('Function is running')
+    time.sleep(2)
+    print('Function Ended')
+
